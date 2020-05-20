558dcb52f918425289668ceab1881f103408ebef
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2020-05-20 13:13:18 -0700

    add basic styling for navbar

--- client/App.jsx
+++ client/App.jsx
@@ -1,15 +1,20 @@
 import React, { useState } from "react";
 import { useRoutes, A } from "hookrouter";
 import Routes from "./router/Routes.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
   //pulling in defined routes within Routes.jsx
+  //the selected route will be displayed through routeResult in return statement below
   const routeResult = useRoutes(Routes);
 
   return (
     <div id="app-wrapper">
       Gather Platform
-      <div id="explore-wrapper">Explore</div>
+
+      {routeResult}
+
+      <Navbar />
     </div>
   );
 }
---
+++ client/router/Routes.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Explore from "../components/Explore.jsx";
+import Login from "../components/Login.jsx";
+
+//all routes for navbar will be defined here
+const routes = {
+    "/Explore": () => <Explore />,
+    "/Login": () => <Login />
+};
+
+export default routes;
