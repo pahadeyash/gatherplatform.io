5d533f01d69d0c1c415f8ac080c21ab63d5a0590
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2020-05-20 12:23:41 -0700

    add hook router, create explore & login page, define routes for router

--- client/App.jsx
+++ client/App.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
+import { useRoutes, A } from "hookrouter";
+import Routes from "./router/Routes.jsx";
 
 function App() {
-    return (
-        <div id="app-wrapper">Gather Platform
+  //pulling in defined routes within Routes.jsx
+  const routeResult = useRoutes(Routes);
 
-        <div id="home-wrapper">Home</div>
-        </div>
-    )
+  return (
+    <div id="app-wrapper">
+      Gather Platform
+      <div id="explore-wrapper">Explore</div>
+    </div>
+  );
 }
 
 export default App;
