ea0090a56cda79b3c89d32b68fe6c0adbc96ec87
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2020-05-27 16:00:03 -0700

    create login and register validation files

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -3,12 +3,30 @@ const Schema = mongoose.Schema;
 
 //define schema for user in creation process
 const userModel = new Schema({
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    first_name: { type: String, required: true },
-    last_name: { type: String, required: true },
-    email: { type: String, required: true },
-    phone: { type: Number, required: true }
+    // username: { type: String, required: true, unique: true },
+    // password: { type: String, required: true },
+    // first_name: { type: String, required: true },
+    // last_name: { type: String, required: true },
+    // email: { type: String, required: true },
+    // phone: { type: Number, required: true }
+
+    //following blog example
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
 })
 
 //exporting user model as User.(...action)
