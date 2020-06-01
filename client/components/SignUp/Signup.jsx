31c3482279b6e2ac5d636b5d373b612509c832e6
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2020-06-01 13:13:47 -0700

    create basic sign up page without functionality

--- client/components/SignUp/Signup.jsx
+++ client/components/SignUp/Signup.jsx
@@ -6,38 +6,42 @@ function Signup() {
     // const [email, setEmail] = useState('');
 
 
-    return <div id="signup-wrapper">
-        <form method="POST" action="/api/users/register" onSubmit={handleSubmit} >
-            <h1>Sign Up</h1>
+    return (
+        <div id="signup-wrapper">
+            <form method="POST" action="/api/users/register"
+            //  onSubmit={handleSubmit} 
+            >
+                <h1>Sign Up</h1>
 
-            <label>Email</label>
-            <input
-                type="text"
-                name="email"
-            // onChange={handleChange}
-            // value={values.name}
-            />
-            <br />
+                <label>Email</label>
+                <input
+                    type="text"
+                    name="email"
+                // onChange={handleChange}
+                // value={values.name}
+                />
+                <br />
 
-            <label>Password</label>
-            <input
-                type="text"
-                name="password"
-            // onChange={handleChange}
-            // value={values.lastName}
-            />
-            <br />
+                <label>Password</label>
+                <input
+                    type="text"
+                    name="password"
+                // onChange={handleChange}
+                // value={values.lastName}
+                />
+                <br />
 
-            <label>Confirm Password</label>
-            <input
-                type="text"
-                name="password"
-            // onChange={handleChange}
-            // value={values.age}
-            />
-            <br />
-            <button type="submit">Submit</button>
-        </form></div>;
+                <label>Confirm Password</label>
+                <input
+                    type="text"
+                    name="password"
+                // onChange={handleChange}
+                // value={values.age}
+                />
+                <br />
+                <button type="submit">Submit</button>
+            </form></div>
+    );
 }
 
 export default Signup;
