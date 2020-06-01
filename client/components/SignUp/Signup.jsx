d004ba48bdf3fd4e88e359763f493f14928a4bde
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2020-06-01 13:07:29 -0700

    create signup route and change route to include proper server endpoints

---
+++ client/components/SignUp/Signup.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+function Signup() {
+    // const [name, setName] = useState('');
+    // const [password, password] = useState('');
+    // const [email, setEmail] = useState('');
+
+
+    return <div id="signup-wrapper">
+        <form method="POST" action="/api/users/register" onSubmit={handleSubmit} >
+            <h1>Sign Up</h1>
+
+            <label>Email</label>
+            <input
+                type="text"
+                name="email"
+            // onChange={handleChange}
+            // value={values.name}
+            />
+            <br />
+
+            <label>Password</label>
+            <input
+                type="text"
+                name="password"
+            // onChange={handleChange}
+            // value={values.lastName}
+            />
+            <br />
+
+            <label>Confirm Password</label>
+            <input
+                type="text"
+                name="password"
+            // onChange={handleChange}
+            // value={values.age}
+            />
+            <br />
+            <button type="submit">Submit</button>
+        </form></div>;
+}
+
+export default Signup;
--- client/router/Routes.jsx
+++ client/router/Routes.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Explore from "../components/Explore.jsx";
 import Login from "../components/Login.jsx";
+import Signup from "../components//SignUp/Signup.jsx";
 
 //all routes for navbar will be defined here
 const routes = {
     "/": () => <Explore />,
-    "/Login": () => <Login />
+    "/api/users/login": () => <Login />,
+    "/api/users/register": () => <Signup />
 };
 
 export default routes;
